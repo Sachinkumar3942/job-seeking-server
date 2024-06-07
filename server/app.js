f83b39e67f8193aa1a12fa995d3157f87358d766
Sachinkumar3942 <sachinkumar3942.9999@gmail.com>
2024-06-07 10:06:06 +0530

    routes updated

--- server/app.js
+++ server/app.js
@@ -14,7 +14,8 @@ dotenv.config({path:"./config/config.env"});
 
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL],
+        // origin: [process.env.FRONTEND_URL],
+        origin: 'http://localhost:3000', 
         methods: ["GET","POST","PUT","DELETE"],
         credentials: true,
     })
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,login,logout } from "../controller/userController.js";
+import { register,login,logout ,getUser} from "../controller/userController.js";
 import {isAuthorized} from "../middleware/auth.js"
 
 const router=express.Router();
@@ -7,6 +7,6 @@ const router=express.Router();
 router.post("/register",register);
 router.post("/login",login);
 router.get("/logout",isAuthorized,logout);
-
+router.get("/getuser",isAuthorized,getUser);
 
 export default router;
