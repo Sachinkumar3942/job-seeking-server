eb1ff62b0469ccd6ddda8df898923046f4439fb2
Sachinkumar3942 <sachinkumar3942.9999@gmail.com>
2024-06-11 09:16:50 +0530

    .

--- server/app.js
+++ server/app.js
@@ -32,6 +32,11 @@ app.use(
     })
 )
 
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/application",applicationRouter);
 app.use("/api/v1/job",jobRouter);
